fc734e1cda4bfb190b64791b1526b07a7984d6be
Fabian Otalora <lfotalora08@gmail.com>
2024-05-03 23:30:55 -0500

    Correccion en donde se borran todos los datos de la tablas en el controlador de Reset

--- app/Http/Controllers/ResetController.php
+++ app/Http/Controllers/ResetController.php
@@ -56,15 +56,24 @@ class ResetController extends Controller
         // Perdidas::all()->delete(); 
 
 
-        DB::statement('ALTER TABLE categorias AUTO_INCREMENT = 1');
-        DB::statement('ALTER TABLE proveedor AUTO_INCREMENT = 1');
-        DB::statement('ALTER TABLE productos AUTO_INCREMENT = 1');
-        DB::statement('ALTER TABLE entradas AUTO_INCREMENT = 1');
-        DB::statement('ALTER TABLE salidas_ventas AUTO_INCREMENT = 1');
-        DB::statement('ALTER TABLE salidas_perdidas AUTO_INCREMENT = 1');
-        DB::statement('ALTER TABLE importes_pagados AUTO_INCREMENT = 1');
-        DB::statement('ALTER TABLE ganancias AUTO_INCREMENT = 1');
-        DB::statement('ALTER TABLE perdidas AUTO_INCREMENT = 1');
+        // DB::statement('ALTER TABLE categorias AUTO_INCREMENT = 1');
+        // DB::statement('ALTER TABLE proveedor AUTO_INCREMENT = 1');
+        // DB::statement('ALTER TABLE productos AUTO_INCREMENT = 1');
+        // DB::statement('ALTER TABLE entradas AUTO_INCREMENT = 1');
+        // DB::statement('ALTER TABLE salidas_ventas AUTO_INCREMENT = 1');
+        // DB::statement('ALTER TABLE salidas_perdidas AUTO_INCREMENT = 1');
+        // DB::statement('ALTER TABLE importes_pagados AUTO_INCREMENT = 1');
+        // DB::statement('ALTER TABLE ganancias AUTO_INCREMENT = 1');
+        // DB::statement('ALTER TABLE perdidas AUTO_INCREMENT = 1');
+        DB::statement('ALTER SEQUENCE categorias_id_categoria_seq RESTART WITH 1');
+        DB::statement('ALTER SEQUENCE proveedor_id_proveedor_seq RESTART WITH 1');
+        DB::statement('ALTER SEQUENCE productos_id_producto_seq RESTART WITH 1');
+        DB::statement('ALTER SEQUENCE entradas_id_entrada_seq RESTART WITH 1');
+        DB::statement('ALTER SEQUENCE salidas_ventas_id_salida_venta_seq RESTART WITH 1');
+        DB::statement('ALTER SEQUENCE salidas_perdidas_id_salida_perdida_seq RESTART WITH 1');
+        DB::statement('ALTER SEQUENCE importes_pagados_id_importe_seq RESTART WITH 1');
+        DB::statement('ALTER SEQUENCE ganancias_id_ganancia_seq RESTART WITH 1');
+        DB::statement('ALTER SEQUENCE perdidas_id_perdida_seq RESTART WITH 1');
 
         return redirect()->route('inicio.index')->with('alert','Todos los datos se han eliminado con éxito.');
         
